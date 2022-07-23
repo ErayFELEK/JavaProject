931533316d3f25a14d147360ee5ea29d29d62c84
Eray <erayfelekbusiness@hotmail.com>
2022-07-23 14:44:08 +0300

    initial commit

---
+++ src/main/java/com/example/galaxydot/GalaxydotApplication.java
@@ -0,0 +1,13 @@
+package com.example.galaxydot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxydotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalaxydotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/galaxydot/GalaxydotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.galaxydot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaxydotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
